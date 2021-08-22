936c5764eb40e19e315181bdec57a2ec8216075b
Deana Bubanj <dbubanj@mathos.hr>
2021-08-22 16:07:03 +0200

    day3

---
+++ Day3/WebApplication1.Repository/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Repository;
+//using WebApplication1.Model;
+
+
+namespace WebApplication1.Repository
+{
+    public class Class1
+    {
+    }
+}
