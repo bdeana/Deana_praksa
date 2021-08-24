c606e71be849d05a5ec2e4874e76808826f89348
Deana Bubanj <dbubanj@mathos.hr>
2021-08-24 23:52:27 +0200

    day7

---
+++ Day7/dbo.Person.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Person] (
+    [OIB]       VARCHAR(20)    NOT NULL,
+    [firstName] VARCHAR (20) NOT NULL,
+    [lastName]  VARCHAR (20) NOT NULL,
+    [mail]      VARCHAR (30) NULL,
+    CONSTRAINT [oib] PRIMARY KEY CLUSTERED ([OIB] ASC)
+);
+
