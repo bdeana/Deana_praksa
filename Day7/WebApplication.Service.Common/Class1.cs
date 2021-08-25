d413b7f331ad180330c2cc0e84a99aa4abbd265f
Deana Bubanj <dbubanj@mathos.hr>
2021-08-26 00:00:59 +0200

    day7

---
+++ Day7/WebApplication.Service.Common/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Model;
+
+namespace WebApplication.Service.Common
+{
+    public class IService
+    {
+        Task<List<Person>> Get();
+        Task<Person> Get(string OIB);
+        Task<bool> Post(string OIB, string mail);
+        Task<bool> Put(string OIB, string firstName, string lastName, string mail);
+        Task<bool> Delete(string OIB);
+
+    }
+}
