dee39f7b0b040220d31b3277954b1014f3c12cab
Deana Bubanj <dbubanj@mathos.hr>
2021-08-25 23:57:31 +0200

    deleted

--- Day7/dbo.Person.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Person] (
-    [OIB]       VARCHAR(20)    NOT NULL,
-    [firstName] VARCHAR (20) NOT NULL,
-    [lastName]  VARCHAR (20) NOT NULL,
-    [mail]      VARCHAR (30) NULL,
-    CONSTRAINT [oib] PRIMARY KEY CLUSTERED ([OIB] ASC)
-);
-
